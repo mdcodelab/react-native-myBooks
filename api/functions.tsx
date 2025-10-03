a659f936f6849d2d5e663ad727e1c6563ebb64a2
miha11-hash <d_mihaela@msn.com>
2025-10-03 11:33:31 +0300

    List ofBooks

---
+++ api/functions.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const endpoint = "https://68dd13757cd1948060ac4c21.mockapi.io/booksstore";
+
+export const getAllBooks = async (onSuccess: (data: any) => void, onError: (error: any) => void) => {
+  try {
+    const result = await axios.get(endpoint);
+    onSuccess(result.data);
+    return result.data;
+  } catch (error) {
+    console.log(error);
+    onError(error);
+  }
+};
